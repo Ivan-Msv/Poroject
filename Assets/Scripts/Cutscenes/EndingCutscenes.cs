d00798c5e8374b00621864ab9db4447386aabdee
Ivan-Msv <s2200853@edu.bc.fi>
2024-09-18 10:06:10 +0300

    Added ending cutscenes

---
+++ Assets/Scripts/Cutscenes/EndingCutscenes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingCutscenes : MonoBehaviour
+{
+    public void FirstEnding()
+    {
+
+    }
+    public void SecondEnding()
+    {
+
+    }
+}
